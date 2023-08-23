8344bd96d8e0b6c2e8e12d05dc05684b7209c8b1
Mishel450 <tomasmishel18@gmai.com>
2023-08-23 13:34:38 -0700

    task-5 done

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -7,3 +7,19 @@ from api.v1.views import app_views
 @app_views.route('/status')
 def json_return():
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats')
+def count_stats():
+    from models.amenity import Amenity
+    from models.city import City
+    from models.place import Place
+    from models.review import Review
+    from models.state import State
+    from models.user import User
+    from models import storage
+    classes = {"amenities": Amenity, "cities": City, "places": Place, "reviews": Review, "states": State, "users": User}
+    r_dict = {}
+    for i in classes:
+        r_dict["{}".format(i)] = storage.count(classes[i])
+    return jsonify(r_dict)
