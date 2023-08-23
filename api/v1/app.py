6a073b4b5d3970761716c97014aac0812524f8d0
Mishel450 <tomasmishel18@gmai.com>
2023-08-23 13:45:00 -0700

    task-6 done

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python3
 """task-0"""
 from models import storage
-from flask import Flask
+from flask import Flask, jsonify
 from api.v1.views import app_views
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+
+
+@app.errorhandler(404)
+def error_handler():
+    return jsonify({"error": "Not found"})
 
 
 @app.teardown_appcontext
