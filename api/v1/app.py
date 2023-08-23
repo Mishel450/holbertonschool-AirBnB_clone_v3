c1ab51d3a3eb85bf58de14bb2cb3044076faf8ec
Mishel450 <tomasmishel18@gmai.com>
2023-08-23 12:59:21 -0700

    task-4 1.2

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,22 +3,15 @@
 from models import storage
 from flask import Flask
 from api.v1.views import app_views
-from os import getenv
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
 
 @app.teardown_appcontext
-def c_storage():
+def c_storage(excepetion):
     """close the storage"""
     storage.close()
-
-
-@app.route('/', strict_slashes=False)
-def h_hbnb():
-    """returns Hello HBNB!"""
-    return "Hello HBNB!"
 
 
 if __name__ == '__main__':
