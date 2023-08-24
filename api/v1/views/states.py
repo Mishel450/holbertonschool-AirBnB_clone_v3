ea094e68db48b0b6d2eb8edf93600c2063e7245a
Mishel450 <tomasmishel18@gmai.com>
2023-08-23 21:00:35 -0700

    task-7 done

---
+++ api/v1/views/states.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python3
+"""task-0"""
+from models import storage
+from models.state import State
+from flask import jsonify, abort, request
+from api.v1.views import app_views
+
+
+@app_views.route('/states/', methods=['GET'])
+@app_views.route('/states/<state_id>', methods=['GET'])
+def states_to_dic(state_id=None):
+    """states in to_dict format"""
+    lista = []
+    for i in storage.all(State):
+        lista.append(storage.all(State)[i].to_dict())
+    if state_id == None:
+        return jsonify(lista)
+    else:
+        for i in lista:
+            if i['id'] == state_id:
+                return jsonify(i)
+        return abort(404)
+
+
+@app_views.route('/states/<state_id>', methods=['DELETE'])
+def delete_state(state_id):
+    """it deletes from objects"""
+    for i in storage.all(State):
+        split_key = i.split(".")
+        if state_id == split_key[1]:
+            del storage.all(State)[i]
+            storage.save()
+            return jsonify({}), 200
+    return abort(404)
+
+
+@app_views.route('/states/', methods=['POST'])
+def post_state():
+    """search and create a state"""
+    data = request.get_json()
+    if not data:
+        error_m = 'Not a JSON'
+        return jsonify(error_m), 400
+    elif 'name' not in data:
+        error_m = ' Missing name'
+        return jsonify(error_m), 400
+    else:
+        obj = State()
+        obj.name = data['name']
+        obj.save()
+        return jsonify(obj.to_dict()), 201
+    
+
+@app_views.route('/states/<state_id>', methods=['PUT'])
+def put_state(state_id):
+    """puts the diccionary in the test"""
+    data = request.get_json()
+    if not data:
+        error_m = 'Not a JSON'
+        return jsonify(error_m), 400
+    else:
+        obj = storage.get(State, state_id)
+        if obj is None:
+            return abort(404)
+        else:
+            obj.name = data['name']
+            obj.save()
+            return jsonify(obj.to_dict()), 201
