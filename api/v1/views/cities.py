2aa41f1510993d474458f3aac42b883f8879d5f1
Mishel450 <tomasmishel18@gmai.com>
2023-08-24 08:51:02 -0700

    task-8 1.6

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -2,11 +2,11 @@
 """task-8"""
 from models import storage
 from models.city import City
-from models.state import State
 from flask import jsonify, abort, request
 from api.v1.views import app_views
 
 
+@app_views.route('states/<state_id>/cities', methods=['GET'])
 @app_views.route('states/<state_id>/cities/', methods=['GET'])
 def get_cities_of_an_id(state_id):
     """searchs in cities and put it in a list if state_id == cities.state_id"""
@@ -43,20 +43,23 @@ def delete_city(city_id):
 @app_views.route('/states/<state_id>/cities', methods=['POST'])
 def post_city(state_id):
     """search and create a state"""
-    obj_data = request.get_json()
-    if not obj_data:
-        abort(400, "Not a JSON")
-    if "name" not in obj_data:
-        abort(400, "Missing name")
-    state = storage.get(State, state_id)
-    """State was a str it needs to be a class"""
-    if state is None:
-        abort(404)
-    obj_data['state_id'] = state.id
-    obj = City(**obj_data)
-    storage.new(obj)
-    storage.save()
-    return jsonify(obj.to_dict()), 201
+    from models.state import State
+    checks = storage.get(State, state_id)
+    if checks is None:
+        return abort(404)
+    if not request.get_json():
+        error_m = 'Not a JSON'
+        return jsonify(error_m), 400
+    data = request.get_json()
+    if 'name' not in data:
+        error_m = ' Missing name'
+        return jsonify(error_m), 400
+    else:
+        data['state_id'] = state_id
+        obj = City(**data)
+        obj.save()
+        return jsonify(obj.to_dict()), 201
+    
 
 @app_views.route('/cities/<city_id>', methods=['PUT'])
 def put_city(city_id):
